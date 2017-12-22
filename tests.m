ad0ff927a11aa5ce98602f9b10427c2f9361e0fd
Meurdok <decanter1u@fst-ats-303-l14.fst.site.univ-lorraine.fr>
2017-12-22 18:37:28 +0100

    calcul distance descripteurs

--- tests.m
+++ tests.m
@@ -4,69 +4,37 @@
 function [recall, precision] = tests()
 img_db_path = './db/';
 img_db_list = glob([img_db_path, '*.gif']);
+img_db_ref = glob('./dbq/*.gif');
 img_db = cell(1);
 label_db = cell(1);
 fd_db = cell(1);
-figure();
+%%figure();
 
 totalX = 0;
 totalY = 0;
 
-
-for im = 1:2;%numel(img_db_list);
-    %totalX = 0;
-    %totalY = 0;
-    img_db{im} = logical(imread(img_db_list{im}));
-    label_db{im} = get_label(img_db_list{im});
-    clf;
-    figure(1);
-    hold on;
-    imshow(img_db{im});
-    disp(label_db{im}); 
-    
-
-    [x, y] = find(img_db{im});
-    mX = round(mean(x));
-    mY = round(mean(y));
-    coupe=180;
-    
-    signatures = zeros(numel(img_db_list),coupe);
-
-    for co = 1:coupe;
-       teta = (co*pi)/180;
-       trouveB = 0;
-       monX = cos(co * 2 * pi()/coupe);
-       monY = sin(co * 2 * pi()/coupe);
-       toastX = mX;
-       toastY = mY;
-       hold on;
-       plot(mY, mX, 'ro');
-       bool=1;
-       %&& toastX < size(img_db{im}, 2) && toastY < size(img_db{im}, 1)
-       
-       while (bool==1 & img_db{im}(round(toastX), round(toastY)) == 1);
-            %coucou = img_db{im}(round(toastX), round(toastY))
-            [m,n] = size(img_db{im});
-            toastX = toastX + monX;
-            toastY = toastY + monY;
-            if (round(toastX) == m || round(toastX) == 0 || round(toastY) == 0 || round(toastY) == n); % tests si ça recontre des bords
-                bool = 0;
-            end
-            hold on;
-            plot(toastY, toastX, 'r+', 'MarkerSize', 3);
-            distance = sqrt((mX - round(toastX))^2  + (mY - round(toastY))^2);
-            signatures(im , co) = distance;
-       end
-       signatures(im,:)
-       hold on;
-       plot(toastY, toastX, 'r.');
-    end
-    
-    X = fft(signatures);
-    X = abs(X)./X(im,0)
-    %hold on;
-    %plot(mY, mX, 'r+', 'MarkerSize', 25);
-    
-    drawnow();
+if(exist('descripteurRef.mat') ~= 2)
+    DescRef = calculdesc(img_db_ref);
+    save('DescripteurRef','DescRef');
 end
 
+load('DescripteurRef.mat','DescRef')
+
+if(exist('descripteurTest.mat') ~= 2)
+    DescTest = calculdesc(img_db_list);
+    save('DescripteurTest','DescTest');
+end
+
+load('DescripteurTest.mat', 'DescTest')
+
+for(im = 1 : 1);%%size(descripteurRef));
+    for(test = 1 : size(DescTest));
+        A = DescRef(im,:);
+        B = DescTest(test,:);
+        diff = A - B;
+        d = norm(diff);
+        distance(im,test) = d;
+    end
+end
+
+distance = sort(distance);
